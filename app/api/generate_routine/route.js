f2807a613d7cb15f6d5f78d6ed321565f9a6e6e2
jjcc <jjcc5000@gmail.com>
2024-11-15 20:09:54 -0500

    cambiando a 3.5

--- app/api/generate_routine/route.js
+++ app/api/generate_routine/route.js
@@ -11,7 +11,7 @@ export async function POST(req) {
 
   try {
     const completion = await openai.chat.completions.create({
-      model: "gpt-4-turbo",
+      model: "gpt-3.5-turbo",
       messages: [
         {
           role: "system",
